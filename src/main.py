68c9dd53ad856be91568a6095f32111c4980fc85
Thijmen <thijmen@thijmentiscool.com>
2026-02-18 22:56:46 +0100

    Cleaner step_types

--- src/main.py
+++ src/main.py
@@ -3,15 +3,39 @@ import asyncio
 
 app = FastAPI()
 
+# Registering steps
+step_handlers = {}
+
+def register_step(step_type):
+    def decorator(func):
+        step_handlers[step_type] = func
+        return func
+    return decorator
+
+@register_step("log")
+async def step_log(step, data):
+    print(f"LOGGING: {step["message"]}")
+    return data
+
+@register_step("set")
+async def step_set(step, data):
+    data[step["key"]] = step["value"]
+    return data
+
+@register_step("delay")
+async def step_delay(step, data):
+    await asyncio.sleep(step.get("seconds", 1))
+
+
+# Executing steps
 async def execute_step(step_id, workflow, data):
     step = workflow["steps"][step_id]
+    step_type = step["type"]
 
-    if step["type"] == "log":
-        print(f"LOGGING: {step["message"]}")
-    elif step["type"] == "delay":
-        await asyncio.sleep(step.get("seconds", 1))
-    elif step["type"] == "set":
-        data[step["key"]] = step["value"]
+    if step_type not in step_handlers:
+        raise Exception(f"Unknown step type: {step_type}")
+
+    data = await step_handlers[step_type](step, data)
 
     next_steps = step.get("next", [])
     if next_steps:
@@ -21,6 +45,7 @@ async def execute_step(step_id, workflow, data):
 
     return data
 
+# Build-in workflow
 workflow = {
     "start": "1",
     "steps": {
@@ -34,6 +59,7 @@ workflow = {
 
 data = {}
 
+# Api endpoints
 @app.get("/")
 def root():
     asyncio.run(execute_step(workflow["start"], workflow, data))
